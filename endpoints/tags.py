3d5085d73b46b04c538f2943aca68c586e81c718
Kevin Z <righttrianglesrkewl3@github.com>
2022-03-09 03:35:25 -0800

    more sensible variable naming

--- endpoints/tags.py
+++ endpoints/tags.py
@@ -5,24 +5,25 @@ import uvicorn
 from fastapi import APIRouter
 from starlette.responses import RedirectResponse
 
+
 session = get_session()
 router = APIRouter()
 
-@router.get("/", name="Home Page", description="API Documentation Page.")
+@router.get("/", name="Home Page", status_code=200, description="API Documentation Page.")
 async def main():
     """API Documentation Page."""
     return "home"
 
-@router.post("/increment", status_code=200,response_model=Tag)
+@router.post("/increment", status_code=200, response_model=Tag)
 async def increment(tag: Tag):
     tags = get_tag_by_name(session, tag)
-    if tags:
-        current_value = get_current(tags)
+    if tag:
+        current_value = get_current(tag)
         return update_tag(session, tag, current_value)
     print(f"[INFO] {tag.name} does not exist...adding..")
     return create_tag(session, tag)
 
-@router.get("/get_tags", status_code=200,response_model=List[Tag])
+@router.get("/get_tags", status_code=200, response_model=List[Tag])
 async def get_tags():
     return get_all_tags(session)
 
