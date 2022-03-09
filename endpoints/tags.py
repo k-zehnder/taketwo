0bb4f65c0bb38266b8ed296f7f7c3b24c237ab26
Kevin Z <righttrianglesrkewl3@github.com>
2022-03-09 03:54:12 -0800

    revert

--- endpoints/tags.py
+++ endpoints/tags.py
@@ -5,26 +5,25 @@ import uvicorn
 from fastapi import APIRouter
 from starlette.responses import RedirectResponse
 
-
 session = get_session()
 router = APIRouter()
 
 @router.get("/", name="Home Page", description="API Documentation Page.")
 async def main():
     """API Documentation Page."""
-    return "home"
+    return RedirectResponse(url="/docs/")
 
-@router.post("/increment", response_model=Tag)
-async def increment(tag: Tag):
-    tag = get_tag_by_name(session, tag)
-    if tag:
-        current_value = get_current(tag)
+@router.post("/increment", status_code=200,response_model=Tag)
+def increment(tag: Tag):
+    tags = get_tag_by_name(session, tag)
+    if tags:
+        current_value = get_current(tags)
         return update_tag(session, tag, current_value)
     print(f"[INFO] {tag.name} does not exist...adding..")
     return create_tag(session, tag)
 
-@router.get("/get_tags", response_model=List[Tag])
-async def get_tags():
+@router.get("/get_tags", status_code=200,response_model=List[Tag])
+def get_tags():
     return get_all_tags(session)
 
 
