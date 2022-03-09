57e892858cfb2dc56ea9928649456efe8910d83d
Kevin Z <righttrianglesrkewl3@github.com>
2022-03-09 12:58:25 -0800

    refactor is_valid_value validator

--- helpers.py
+++ helpers.py
@@ -11,10 +11,12 @@ VALID_NAME_CHARACTERS = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
 VALID_NAME_RANGE = [str(i) for i in range(3, 16)]
 
 
-def is_valid(query):
+def is_valid_digits(query):
     matched = re.findall('\d+', query)
     return all(m in VALID_NAME_RANGE for m in matched)
 
+def is_valid_chars(query):
+    return any(char not in VALID_NAME_CHARACTERS for char in query if char.isalpha())
 
 class Tag(BaseModel):
     name: str
@@ -22,19 +24,19 @@ class Tag(BaseModel):
 
     @validator("name")
     def is_valid_name(cls, name):
-        print(is_valid(name))
-        is_valid_name = is_valid(name)
-        if not is_valid_name:
-             raise ValueError("Bad name, digits are {3, 15}.")       
-
-        if any(char not in VALID_NAME_CHARACTERS for char in name if char.isalpha()):
+        print(is_valid_digits(name))
+        valid_name = is_valid_digits(name)
+        if not valid_name:
+             raise ValueError("Bad name, digits are {3, 15}.") 
+        valid_chars = is_valid_chars(name)      
+        if valid_chars:
             raise ValueError("Bad name, must be character.")
         return name
 
     @validator("value")
     def is_valid_value(cls, value):
         """Validator to check whether value is valid"""
-        if not isinstance(value, int) or not 0 <= value < 10:
+        if not 0 <= value < 10:
             raise ValueError("Bad value, must be integer and 0 <= integer < 10")
         return value
 
