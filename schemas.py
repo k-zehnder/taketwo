d883123ac04d9dbe5e8bb62b3f16cba066b68bac
Kevin Z <righttrianglesrkewl3@github.com>
2022-03-09 23:22:57 -0800

    refactor Tag

--- schemas.py
+++ schemas.py
@@ -20,20 +20,19 @@ class Tag(BaseModel):
 
     @validator("name")
     def is_valid_name(cls, name):
-        print(is_valid_digits(name))
+        valid_chars = is_valid_chars(name)   
+        if not valid_chars:  
+            raise ValueError("Invalid chars")  
         valid_digits = is_valid_digits(name)
         if not valid_digits:
-             raise ValueError("Bad name, digits are {3, 15}.") 
-        valid_chars = is_valid_chars(name)      
-        if not valid_chars:
-            raise ValueError("Bad name, must be character.")
+             raise ValueError("Invalid digits") 
         return name
 
     @validator("value")
     def is_valid_value(cls, value):
         """Validator to check whether value is valid"""
         if not 0 <= value < 10:
-            raise ValueError("Bad value, must be integer and 0 <= integer < 10")
+            raise ValueError("Invalid value, must be integer and 0 <= integer < 10")
         return value
 
     class Config:
