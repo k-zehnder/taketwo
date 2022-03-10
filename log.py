a15e5268cd83a874f2de924aebae4b9769515d9a
Kevin Z <righttrianglesrkewl3@github.com>
2022-03-10 01:45:25 -0800

    cleanup log

--- log.py
+++ log.py
@@ -3,17 +3,17 @@ import google.cloud.logging
 from datetime import datetime, timedelta, timezone
 
 
-client = google.cloud.logging.Client()
-logger = client.logger(name="post_count")
-
 # query and print all matching log
-def get_logs(filter_str):
+def get_logs(logger, filter_str):
     for entry in logger.list_entries(filter_=filter_str):
         print(entry)
         print()
         print()
 
 if __name__ == "__main__":
+    client = google.cloud.logging.Client()
+    logger = client.logger(name="post_count")
+
     yesterday = datetime.now(timezone.utc) - timedelta(minutes=100)
     # Cloud Logging expects a timestamp in RFC3339 UTC "Zulu" format
     # https://cloud.google.com/logging/docs/reference/v2/rest/v2/LogEntry
@@ -23,4 +23,5 @@ if __name__ == "__main__":
     filter_str = (
         f'timestamp>="{yesterday.strftime(time_format)}"'
     )
-    get_logs(filter_str)
+    
+    get_logs(logger, filter_str)
