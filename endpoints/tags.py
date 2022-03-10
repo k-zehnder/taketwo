fd00277cdba249ada7eaa7db88fdc1045a1a2b36
Kevin Z <righttrianglesrkewl3@github.com>
2022-03-09 20:47:55 -0800

    test working

--- endpoints/tags.py
+++ endpoints/tags.py
@@ -3,17 +3,12 @@ from fastapi import FastAPI
 from helpers import Tag, get_all_tags, get_session, create_tag, get_current_value, update_tag, get_tag_by_name, sum_all_tags
 from fastapi import APIRouter
 from starlette.responses import RedirectResponse
+from typing import Set
 
-import google.cloud.logging
-import logging
-from datetime import datetime, timedelta, timezone
-import os
 
 session = get_session()
 router = APIRouter()
 
-# client = google.cloud.logging.Client()
-# client.setup_logging()# use Python’s standard logging library to send logs to GCP
 
 @router.get("/", name="Home Page", description="API Documentation Page.")
 async def main():
@@ -33,10 +28,7 @@ def increment(tag: Tag):
 def get_tags():
     tag_sum = sum_all_tags(session)
     print(f'[INFO] sum: {tag_sum}')
-    # logging.info("logging sum", extra={
-    #     "labels": {"type": "app_health"},
-    #     "trace": "01234" # + uuid
-    # })
+    # logger.log(f"Total request count: {tag_sum}"
     return get_all_tags(session)
 
 
