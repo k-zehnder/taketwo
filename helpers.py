73cafba7c916d176dc8f97fce5a09e03bab51385
Kevin Z <righttrianglesrkewl3@github.com>
2022-03-09 22:48:18 -0800

    move Tag to schemas.py

--- helpers.py
+++ helpers.py
@@ -6,6 +6,8 @@ from pydantic import BaseModel, validator
 import string
 import re
 
+from schemas import Tag
+
 
 VALID_NAME_CHARACTERS = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ' 
 VALID_NAME_RANGE = [str(i) for i in range(3, 16)]
@@ -18,30 +20,30 @@ def is_valid_digits(query):
 def is_valid_chars(query):
     return all(char in VALID_NAME_CHARACTERS for char in query if char.isalpha())
 
-class Tag(BaseModel):
-    name: str
-    value: int
+# class Tag(BaseModel):
+#     name: str
+#     value: int
 
-    @validator("name")
-    def is_valid_name(cls, name):
-        print(is_valid_digits(name))
-        valid_digits = is_valid_digits(name)
-        if not valid_digits:
-             raise ValueError("Bad name, digits are {3, 15}.") 
-        valid_chars = is_valid_chars(name)      
-        if not valid_chars:
-            raise ValueError("Bad name, must be character.")
-        return name
+#     @validator("name")
+#     def is_valid_name(cls, name):
+#         print(is_valid_digits(name))
+#         valid_digits = is_valid_digits(name)
+#         if not valid_digits:
+#              raise ValueError("Bad name, digits are {3, 15}.") 
+#         valid_chars = is_valid_chars(name)      
+#         if not valid_chars:
+#             raise ValueError("Bad name, must be character.")
+#         return name
 
-    @validator("value")
-    def is_valid_value(cls, value):
-        """Validator to check whether value is valid"""
-        if not 0 <= value < 10:
-            raise ValueError("Bad value, must be integer and 0 <= integer < 10")
-        return value
+#     @validator("value")
+#     def is_valid_value(cls, value):
+#         """Validator to check whether value is valid"""
+#         if not 0 <= value < 10:
+#             raise ValueError("Bad value, must be integer and 0 <= integer < 10")
+#         return value
 
-    class Config:
-        orm_mode = True
+#     class Config:
+#         orm_mode = True
 
 
 def update_tag(session, tag, current_value):
@@ -81,3 +83,20 @@ def get_current_value(tags):
 def log_tag_sum(logger, tag_sum):
     logger.log(f"[TAG_TOTAL] {tag_sum}", resource={"type":"global", 
         "labels":{"total" : "update"}})
+
+"""
+Example full log payload sent to GCP from 'log_tag_sum' function above.
+{
+    insertId: "1l5w75vfh4rv49"
+    logName: "projects/project3-343609/logs/post_count"
+    receiveTimestamp: "2022-03-10T06:38:49.656625022Z"
+    resource: {
+    labels: {
+        project_id: "project3-343609"
+    }
+        type: "global"
+    }
+    textPayload: "[TAG_TOTAL] 1"
+    timestamp: "2022-03-10T06:38:49.656625022Z"
+}
+"""
