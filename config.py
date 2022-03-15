ae35ad751ab2fa798338fd1f68a8978da15b48c4
Kevin Z <righttrianglesrkewl3@github.com>
2022-03-15 14:05:53 -0700

    fix typo pointing to db in config

--- config.py
+++ config.py
@@ -3,5 +3,5 @@ import os
 VALID_NAME_CHARACTERS ="abcdefghijklmnopqrstuvwxyz"
 VALID_NAME_RANGE=[str(i) for i in range(3, 16)]
 LOG_NAME="post_count" 
-TAG_DB=u"tabdb"
+TAG_DB=u"tagdb"
 CREDS=os.environ.get("GOOGLE_APPLICATION_CREDENTIALS")
