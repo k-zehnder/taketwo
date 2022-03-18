ccf2b6d82444ab05058b07ad0595378e65f961f3
Kevin Z <righttrianglesrkewl3@github.com>
2022-03-17 19:31:44 -0700

    use creds to debug issue with GCP

--- config.py
+++ config.py
@@ -4,4 +4,5 @@ VALID_NAME_CHARACTERS ="abcdefghijklmnopqrstuvwxyz"
 VALID_NAME_RANGE=[str(i) for i in range(3, 16)]
 LOG_NAME="post_count" 
 TAG_DB=u"tagdb"
-CREDS=os.environ.get("GOOGLE_APPLICATION_CREDENTIALS")
+# CREDS=os.environ.get("GOOGLE_APPLICATION_CREDENTIALS")
+CREDS="./config.json"
